9cd84c1afb8e317a1f05c0e4e94fedc84d494fbe
Tomy Miranda <tomasmiranda98@hotmail.com>
2021-04-13 00:45:23 -0300

    al ejecutar los test juntos pasan todos

--- src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
@@ -50,6 +50,8 @@ class VendedorTest : DescribeSpec({
     val cordoba = Provincia(2000000)
     val villaDolores = Ciudad(cordoba)
     val viajante = Viajante(listOf(misiones))
+    val viajante2 = Viajante(listOf(cordoba))
+    val viajante3 = Viajante(listOf(cordoba))
 
     describe("puedeTrabajarEn") {
       it("una ciudad que pertenece a una provincia habilitada") {
@@ -71,58 +73,54 @@ class VendedorTest : DescribeSpec({
     }
     describe("es un vendedor"){
       it("es firme"){
-      viajante.agregarCertificacion(Certificacion(true,5000))
-      viajante.agregarCertificacion(Certificacion(true,5000))
-      viajante.agregarCertificacion(Certificacion(false,2000))
-      viajante.agregarCertificacion(Certificacion(false,2000))
+        viajante.agregarCertificacion(Certificacion(true,50))
+        viajante.agregarCertificacion(Certificacion(true,50))
+        viajante.agregarCertificacion(Certificacion(false,20))
 
-      viajante.esFirme().shouldBeTrue()
-    }
+        viajante.esFirme().shouldBeTrue()
+      }
       it("no seria firme"){
-        viajante.agregarCertificacion(Certificacion(true,5))
-        viajante.esFirme().shouldBeFalse()
+          viajante2.esFirme().shouldBeFalse()
       }
 
       it("es versatil"){
-        viajante.agregarCertificacion(Certificacion(true,5000))
-        viajante.agregarCertificacion(Certificacion(true,5000))
-        viajante.agregarCertificacion(Certificacion(false,2000))
+        viajante.agregarCertificacion(Certificacion(true,50))
+        viajante.agregarCertificacion(Certificacion(true,50))
+        viajante.agregarCertificacion(Certificacion(false,20))
 
         viajante.esVersatil().shouldBeTrue()
       }
       it("no es versatil porque no tiene una certificacion que no es de producto"){
-        viajante.agregarCertificacion(Certificacion(true,5000))
-        viajante.esVersatil().shouldBeFalse()
+          viajante2.agregarCertificacion(Certificacion(true,5000))
+          viajante2.agregarCertificacion(Certificacion(true,5000))
+          viajante2.agregarCertificacion(Certificacion(true,5000))
+
+          viajante2.esVersatil().shouldBeFalse()
       }
       it("no es versatil porque no tiene una certificacion de producto"){
-        viajante.agregarCertificacion(Certificacion(false,5000))
-        viajante.esVersatil().shouldBeFalse()
+
+          viajante3.agregarCertificacion(Certificacion(false,5000))
+          viajante3.agregarCertificacion(Certificacion(false,5000))
+          viajante3.agregarCertificacion(Certificacion(false,5000))
+          viajante3.agregarCertificacion(Certificacion(false,5000))
+
+          viajante3.esVersatil().shouldBeFalse()
       }
 
     }
-  describe("las certificaciones que tenga"){
-    it("den un puntaje de 12K"){
-      viajante.agregarCertificacion(Certificacion(true,5000))
-      viajante.agregarCertificacion(Certificacion(true,5000))
-      viajante.agregarCertificacion(Certificacion(false,2000))
+    describe("las certificaciones que tenga"){
+        it("den un puntaje de 240"){
 
-      ( viajante.puntajeCertificaciones()==12000).shouldBeTrue()
-    }
-    it("sean 2 de de producto"){
-      viajante.agregarCertificacion(Certificacion(true,5000))
-      viajante.agregarCertificacion(Certificacion(true,5000))
-      viajante.agregarCertificacion(Certificacion(false,2000))
+            (viajante.puntajeCertificaciones()==240).shouldBeTrue()
+        }
+        it("sean 4 de de producto"){
 
-      (viajante.certificacionesDeProducto() == 2).shouldBeTrue()
+            (viajante.certificacionesDeProducto() == 4).shouldBeTrue()
+        }
+        it("sea 2 de no producto"){
+            (viajante.otrasCertificaciones() == 2).shouldBeTrue()
+        }
     }
-    it("sea 1 de no producto"){
-      viajante.agregarCertificacion(Certificacion(true,5000))
-      viajante.agregarCertificacion(Certificacion(true,5000))
-      viajante.agregarCertificacion(Certificacion(false,2000))
-
-      (viajante.otrasCertificaciones() == 1).shouldBeTrue()
-    }
-  }
   }
 
   describe("ComercioCorresponsal"){
@@ -136,7 +134,7 @@ class VendedorTest : DescribeSpec({
         comercio.puedeTrabajarEn(hurlingham).shouldBeTrue()
       }
       it("no en san ignasio"){
-        comercio.puedeTrabajarEn(sanIgnacio).shouldBeFalse()
+        comercio.puedeTrabajarEn(villaDolores).shouldBeFalse()
       }
     }
     describe("caso de ser influyentes "){
@@ -159,81 +157,82 @@ class VendedorTest : DescribeSpec({
 
   }
   //etapa3
-  describe("CentroDeDistribucion"){
+  describe("CentroDeDistribucion") {
 
-    val viajante = Viajante(listOf(misiones))
-    val viajante2 = Viajante(listOf(cordoba))
-    val viajanteNuevo = Viajante(listOf(misiones,cordoba))
+      val viajante = Viajante(listOf(misiones))
+      val viajante2 = Viajante(listOf(cordoba))
+      val viajanteNuevo = Viajante(listOf(misiones, cordoba))
 
-    val obera = Ciudad(misiones)
-    val vendedorFijo = VendedorFijo(obera)
+      val obera = Ciudad(misiones)
+      val vendedorFijo = VendedorFijo(obera)
 
-    vendedorFijo.agregarCertificacion(Certificacion(true,5000))
+      vendedorFijo.agregarCertificacion(Certificacion(false,50))
+      vendedorFijo.agregarCertificacion(Certificacion(false,50))
 
-    val vendedoresDeCentro = mutableListOf<Vendedor>()
-    vendedoresDeCentro.add(viajante)
-    vendedoresDeCentro.add(viajante2)
-    vendedoresDeCentro.add(vendedorFijo)
+      val vendedoresDeCentro = mutableListOf<Vendedor>()
+      vendedoresDeCentro.add(viajante)
+      vendedoresDeCentro.add(viajante2)
+      vendedoresDeCentro.add(vendedorFijo)
 
-    val centro = CentroDeDistribucion(villaDolores,vendedoresDeCentro)
+      val centro = CentroDeDistribucion(villaDolores, vendedoresDeCentro)
 
-    describe("puedeQue"){
-        it("los vendedores que tiene el centro son 3"){
-          (centro.vendedores.size == 3).shouldBeTrue()
-        }
-        it("se agrega un vendedor y ahora el centro tiene 4"){
-          centro.agregarUnVendedor(viajanteNuevo)
-          (centro.vendedores.size == 4).shouldBeTrue()
-        }
-        it("no se agregue un vendedor y rompe") {
-          shouldThrowAny { centro.agregarUnVendedor(viajante) }
-        }
-      describe("caso de vendedor estrella"){
-        it("el vendedor estrella es el vendedorFijo"){
-          (centro.vendedorEstrella()==vendedorFijo).shouldBeTrue()
-        }
+      describe("puedeQue") {
+          it("los vendedores que tiene el centro son 4") {
+              (centro.vendedores.size == 3).shouldBeTrue()
+          }
+          it("se agrega un vendedor y ahora el centro tiene 4") {
+              centro.agregarUnVendedor(viajanteNuevo)
+              (centro.vendedores.size == 4).shouldBeTrue()
+          }
+          it("no se agregue un vendedor y rompe") {
+              shouldThrowAny { centro.agregarUnVendedor(viajante) }
+          }
 
-        it("el vendedor estrella no es el viajante"){
-          (centro.vendedorEstrella()==viajante).shouldBeFalse()
-        }
+      }
+      describe("caso de vendedor estrella") {
+          it("el vendedor estrella es el vendedorFijo") {
+              (centro.vendedorEstrella() == vendedorFijo).shouldBeTrue()
+          }
+
+          it("el vendedor estrella no es el viajante") {
+              (centro.vendedorEstrella() == viajante).shouldBeFalse()
+          }
       }
       describe("caso de si es robusto"){
-        it("no es robusto el centro"){
-          centro.esRobusto().shouldBeFalse()
-        }
-        it("al repartir certificaciones se vuelve robusto"){
-          centro.agregarUnVendedor(viajanteNuevo)
-          centro.repartirCertificaciones(Certificacion(true,500))
+            it("no es robusto el centro"){
+                centro.esRobusto().shouldBeFalse()
+            }
+            it("al repartir certificaciones se vuelve robusto"){
 
-          centro.esRobusto().shouldBeTrue()
-        }
+                centro.repartirCertificaciones(Certificacion(true,50))
+
+                centro.esRobusto().shouldBeTrue()
+            }
       }
       describe("caso vendedor estrella firme"){
-        it("es firme el vendedor estrella"){
-          centro.repartirCertificaciones(Certificacion(true,500))
-          val estrella = centro.vendedorEstrella()
-          if (estrella != null) {
-            estrella.esFirme().shouldBeTrue()
-          }
-        }
-        it("no es firme el vendedor estrella"){
-          val estrella = centro.vendedorEstrella()
-          if (estrella != null) {
-            estrella.esFirme().shouldBeTrue()
-          }
-        }
+            it("es firme el vendedor estrella"){
+                centro.repartirCertificaciones(Certificacion(true,500))
+                val estrella = centro.vendedorEstrella()
+                if (estrella != null) {
+                    estrella.esFirme().shouldBeTrue()
+                }
+            }
+            it("no es firme el vendedor estrella"){
+                val estrella = centro.vendedorEstrella()
+                if (estrella != null) {
+                    estrella.esFirme().shouldBeTrue()
+                }
+            }
       }
-
-
       describe("caso en genericos"){
-        it("el vendedorFijo es un vendedor generico"){
-          (centro.vendedoresGenericos() == listOf(vendedorFijo)).shouldBeTrue()
-        }
+          it("el vendedorFijo es un vendedor generico"){
+                (centro.vendedoresGenericos() == listOf(vendedorFijo)).shouldBeTrue()
+            }
           it("el viajante no es un vendedor generico"){
-            (centro.vendedoresGenericos() == listOf(vendedorFijo)).shouldBeFalse()
+            (centro.vendedoresGenericos() == listOf(viajante)).shouldBeFalse()
           }
       }
 
     }
-  }
+
 })
